81b267627ed320b61921380380192c353670f9aa
Kelwin Roberto Gamez Barradas <kgamez@belatrix.com>
2019-06-04 13:50:25 -0500

    course awsm@pluralsigt

--- assets/js/make.js
+++ assets/js/make.js
@@ -15,7 +15,7 @@ function loadImages () {
   $.each(toppings, function (key, val) {
     var img = new Image();
     img.setAttribute('crossOrigin', 'anonymous');
-    img.src = '/assets/toppings/' + val.image;
+    img.src = '//pizza-luvrs-kelwin-gamez.s3-us-west-2.amazonaws.com/toppings/' + val.image;
     val.img = img;
   });
 }
